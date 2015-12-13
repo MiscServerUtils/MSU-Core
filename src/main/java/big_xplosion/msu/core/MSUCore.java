ade2d6d4e26054c92d17912dba56c6a2cb574180
BigXplosion <bigXplosion44@gmail.Com>
2015-12-13 19:52:26 +0100

    Fix registry

--- src/main/java/big_xplosion/msu/core/MSUCore.java
+++ src/main/java/big_xplosion/msu/core/MSUCore.java
@@ -4,6 +4,7 @@ import big_xplosion.msu.core.helper.RegistryHelper;
 import big_xplosion.msu.core.lib.Constants;
 import org.spongepowered.api.Game;
 import org.spongepowered.api.event.Listener;
+import org.spongepowered.api.event.game.state.GameConstructionEvent;
 import org.spongepowered.api.event.game.state.GamePreInitializationEvent;
 import org.spongepowered.api.plugin.Plugin;
 
@@ -13,12 +14,18 @@ public class MSUCore {
 	private static MSUCore plugin;
 	private static Game game;
 
-	public static RegistryHelper registry = RegistryHelper.createRegistryHelper(plugin, game);
+	public RegistryHelper registry;
 
 	@Listener
-	public void preInit(GamePreInitializationEvent event) {
+	public void onConstruction(GameConstructionEvent event) {
 		plugin = this;
 		game = event.getGame();
+		registry = RegistryHelper.createRegistryHelper(plugin, game);
+	}
+
+	@Listener
+	public void onPreInit(GamePreInitializationEvent event) {
+
 	}
 
 	public static MSUCore getPlugin() {
